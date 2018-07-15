7692d90d60a6db43ddf677e152bcb0410502cdc0
Rory Dungan <rorydungan@gmail.com>
2018-07-15 14:35:10 +1000

    Simplify code in ArtistsSelect

--- src/ArtistsSelect.js
+++ src/ArtistsSelect.js
@@ -2,12 +2,10 @@ import React from 'react'
 import Select, { Option } from 'antd/lib/select'
 
 export default function ArtistsSelect (props) {
-  const artists = props.artists || []
-
   return (
     <Select style={{ width: '100%' }} onChange={ props.onChange }>
       {
-        artists.map(a =>
+        props.artists.map(a =>
           <Option key={ a.id }>{ a.name }</Option>
         )
       }
